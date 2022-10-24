1a6dd9fbf2091f43cc0ba7c0bf1ca3efa186cc77
Oksanaka848 <okmitka@mail.ru>
2022-10-24 21:31:37 +0300

    ch

--- js.js
+++ js.js
@@ -8,8 +8,19 @@
     let result = document.getElementById("result");
     let button = document.getElementById("button");
     button.onclick = sumInput;
+let i=0;
 
 sumInput  = () => {
-
-}
+    if ($("#num").val() != "") {
+        $("#error").hide();
+        $("#error").text("");
+        var storedValue = $("#num").val();
+        $("#number").show();
+        $("#number").text(storedValue);
+      } else {
+        $("#error").show();
+        $("#error").text("Please complete the above field.");
+      }
+    }
+    let n1 = parseInt(document.getElementById("num").value);
 document.getElementById("result").innerHTML = (+n1 - +n2);
