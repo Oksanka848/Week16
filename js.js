74065c90d3c4aa69d6c73c100f54aa748e7d673e
Oksanaka848 <okmitka@mail.ru>
2022-10-24 21:01:54 +0300

    ch

---
+++ js.js
@@ -0,0 +1,15 @@
+/*1. Напишите функцию `sumInput()`, которая:
+    - Просит пользователя ввести значения, используя текстовое поле `input` и сохраняет их в массив
+    - Когда пользователь нажмёт кнопку «Готово»
+    - Выводит получившийся массив в отсортированном
+    по возрастанию виде
+    - Подсчитывает и возвращает сумму элементов массива*/
+    let num = document.getElementById("num");
+    let result = document.getElementById("result");
+    let button = document.getElementById("button");
+    button.onclick = sumInput;
+
+sumInput  = () => {
+
+}
+document.getElementById("result").innerHTML = (+n1 - +n2);
