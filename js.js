b12bc23b8ecbe26e061d117edecf28ac595ced5c
Oksanaka848 <okmitka@mail.ru>
2022-10-25 21:55:37 +0300

    ch

--- js.js
+++ js.js
@@ -1,26 +1,45 @@
 /*1. Напишите функцию `sumInput()`, которая:
-    - Просит пользователя ввести значения, используя текстовое поле `input` и сохраняет их в массив
+    - Просит пользователя ввести значения, 
+    используя текстовое поле `input` и сохраняет их в массив
     - Когда пользователь нажмёт кнопку «Готово»
     - Выводит получившийся массив в отсортированном
     по возрастанию виде
     - Подсчитывает и возвращает сумму элементов массива*/
-    let num = document.getElementById("num");
+    /*et num = document.getElementById("num");
     let result = document.getElementById("result");
     let button = document.getElementById("button");
     button.onclick = sumInput;
 let i=0;
 
-sumInput  = () => {
-    if ($("#num").val() != "") {
-        $("#error").hide();
-        $("#error").text("");
-        var storedValue = $("#num").val();
-        $("#number").show();
-        $("#number").text(storedValue);
-      } else {
-        $("#error").show();
-        $("#error").text("Please complete the above field.");
-      }
-    }
-    let n1 = parseInt(document.getElementById("num").value);
+
 document.getElementById("result").innerHTML = result;
+
+let num = document.getElementById("num");
+let masnum = str.split (' ');
+let result = document.getElementById("result");*/
+    /*let button = document.getElementById("button");
+    button.onclick = sumInput;
+    function sumInput() {
+      let num = document.getElementById("num");
+      console.log(typeof num);
+      let mas = num.split (' ');
+      console.log(mas);
+      mas.sort(function (a, b) {
+          return a - b;
+});
+document.getElementById("result").innerHTML= (mas);
+
+    }*/
+    let button = document.getElementById("button");
+    button.onclick = sumInput;
+    function sumInput() {
+    let mas = document.getElementById("num");
+    mas.toString().split (',');
+    console.log(mas);
+    
+    mas =[0];
+    mas.sort(function (a, b) {
+      return a - b;
+});
+    document.getElementById("result").innerHTML= (mas);
+  }
