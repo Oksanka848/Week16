e216a75e1e1c856612cecba3beec46edb15f12d7
Oksanka848 <okmitka@mail.ru>
2022-10-26 14:45:15 +0000

    ch

--- car/bootstrap/boots.js
+++ car/bootstrap/boots.js
@@ -1,16 +1,17 @@
-$(document).ready(function() {
-    $(".total").text("Итоговая стоимость: тысяч рублей (вкл.НДС)");
-  });
+/*$(document).ready(function() {
+    document.getElementById('total').textContent = "Итоговая стоимость: тысяч рублей (вкл.НДС)";
+  });*/
 
-$('input,select').on('change', refreshTotal);
 
-function refreshTotal() {
-    var brand = +$('#brand').val();
-    var model = +$('#model').val();
-    var bodytype = +$('#bodytype').val();
-    var mileage = +$('#mileage').val();
-    var gas = +$('input[name="gas"]:checked', '#gas').val();
-    var age = +$('input[name="age"]:checked','#age').val();
+  document.getElementById('button').onclick = function refreshTotal() {
+
+    var brand = document.getElementById ('brand').value [0];
+    var model = document.getElementById ('model').value [0];
+    var bodytype = document.getElementById ('bodytype').value [0];
+    var mileage = document.getElementById ('mileage').value [0];
+    var gas = document.querySelector('input[type=checkbox][name="gas"]:checked').value;
+    var age = document.querySelector('input[type=checkbox][name="age"]:checked').value;
+    
 
     if (isNaN(brand)) brand = 0;
     if (isNaN(model)) model = 0;
@@ -20,5 +21,5 @@ function refreshTotal() {
     if (isNaN(age)) age = 0;
 
     var total = brand + model + bodytype + mileage + gas + age;
-    $(".total").text("Итоговая стоимость: " + total + " (вкл.НДС)");
+    document.getElementById('total').textContent ="Итоговая стоимость: " + total + " (вкл.НДС)";
 }
