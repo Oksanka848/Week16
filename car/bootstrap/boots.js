6b530eb12fca30586ac2dca390aa3d8d88a7ac29
Oksanka848 <okmitka@mail.ru>
2022-10-26 13:36:42 +0000

    ch

--- car/bootstrap/boots.js
+++ car/bootstrap/boots.js
@@ -1,22 +1,24 @@
 $(document).ready(function() {
-    $(".total").text("Total: $0 /month");
+    $(".total").text("Итоговая стоимость: тысяч рублей (вкл.НДС)");
   });
 
 $('input,select').on('change', refreshTotal);
 
 function refreshTotal() {
-    var sex = +$('input[name="sex"]:checked', '#sex').val();
-    var age = +$('input[name="age"]:checked', '#age').val();
-    var car = +$('#car').val();
-    var year = +$('#year').val();
-    var citations = +$('#citations').val();
+    var brand = +$('#brand').val();
+    var model = +$('#model').val();
+    var bodytype = +$('#bodytype').val();
+    var mileage = +$('#mileage').val();
+    var gas = +$('input[name="gas"]:checked', '#gas').val();
+    var age = +$('input[name="age"]:checked','#age').val();
 
-    if (isNaN(sex)) sex = 0;
+    if (isNaN(brand)) brand = 0;
+    if (isNaN(model)) model = 0;
+    if (isNaN(bodytype)) bodytype = 0;
+    if (isNaN(mileage)) mileage = 0;
+    if (isNaN(gas)) gas = 0;
     if (isNaN(age)) age = 0;
-    if (isNaN(car)) car = 0;
-    if (isNaN(year)) year = 0;
-    if (isNaN(citations)) citations = 0;
 
-    var total = sex + age + car + year + citations;
-    $(".total").text("Total: $" + total + " /month");
+    var total = brand + model + bodytype + mileage + gas + age;
+    $(".total").text("Итоговая стоимость: " + total + " (вкл.НДС)");
 }
