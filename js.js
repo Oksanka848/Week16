c3983d2ffb7e188729b73097d175f7c06855f52a
Oksanaka848 <okmitka@mail.ru>
2022-10-30 21:40:46 +0300

    ch

--- js.js
+++ js.js
@@ -5,38 +5,28 @@
     - Выводит получившийся массив в отсортированном
     по возрастанию виде
     - Подсчитывает и возвращает сумму элементов массива*/
-    /*var str = document.getElementById("numbers")+ ' ';
-  var arr = [str];
-  var arr = str.split(' ');
-  
-  arr.sort(function (a, b) {
-    return a - b;
-});*/
-/*let mas = [];
-          
-            for(i=0;i<=3;i++){
-                mas[i]=nums[i].value; // Из инпутов в массив
-            }
-            arr.sort((a, b) => b - a);
-            var result = {};
-    var elements = document.forms.foo.getElementsByTagName("input");
-    for(var i = 0; i < elements.length; i++)
-    {
-       /* do whatever you need to do with each input 
-    }let number = numbers.split(' ')
-    var arr = str.split(' ');*/
-    var numbs = document.getElementById('numbers').value;
-    var sort = document.getElementById('sort');
-    var sum  = document.getElementById('result');
-    var button = document.getElementById('button');
+    
+    let button = document.getElementById('button');
     button.onclick = sumInput;
+    let numbs = document.getElementById('numbers');
+    let all = document.getElementById('sort');
+    let result  = document.getElementById('result');
+    let numbers = [];
     
-    var numbers = [numbs];
-    
-
-function sumInput() {
-  numbers.sort((a, b) => b - a);
-  document.getElementById('result').innerHTML=numbers;}
-
-
+    for( let i = 0; i < numbs.length; i++ ){
+      numbers.push( numbs[i].value );
+      small[i].addEventListener('input', function(){
+        numbers[i] = this.value; 
+        console.log(numbers);
         
+      });
+    }  
+    console.log(numbers);
+function sumInput() {
+  result.value=numbers.reduce(function(sum, current) {
+    return sum + current;
+  }, 0);
+  all.value = numbers.join(', ');
+  document.getElementById('result').innerHTML=result.value;
+  document.getElementById('result').innerHTML=all.value;
+}
--- js2.js
+++ js2.js
@@ -1,90 +1,20 @@
-let sum   = document.getElementById('sum');
-let all   = document.getElementById('all');
-
-let small = document.querySelectorAll('.input-small'); 
-let numbers = []; // Массив, куда запишутся все значения инпутов
-
-for( let i = 0; i < small.length; i++ ){
-  numbers.push( small[i].value ); // (*1)
+/*var str = document.getElementById("numbers")+ ' ';
+  var arr = [str];
+  var arr = str.split(' ');
   
-  small[i].addEventListener('input', function(){
-    numbers[i] = this.value; 
-    // При вводе чисел в любом инпуте, не будем пересобирать все значения, 
-    // а только изменим конкретный элемент массива. 
-    // this - указывает на инпут, в котором печатают (который запускает эту функцию)
-
-    // Все значения в массиве обновлены, можно обновить и результаты:
-    updateResults();
-  });
-}
-updateResults();
-
-/************/
-
-function updateResults(){
-  sum.value = sumArr( numbers );
-  all.value = numbers.join(', ');
-}
-
-function sumArr(arr){
-  let x = 0;
-  for( let i = 0; i < arr.length; i++ ){
-    x += +arr[i]; // (*2)
-  }
-  return x;
-}
-
-
-/*function sumInput() {
-      //res.value = sumArr( numbers );
-      //sort.value = numbers.join(', ');}
-      /*numbers.sort(function(a, b) {
-        return a - b;
-      });
-     
-      let x = 0;
-      for( let i = 0; i < sort.length; i++ ){
-        x += +sort[i]; // (*2)
-      }
-      return x;
-    }
-    
-   /* function sumArr(numbers){
-      let x = 0;
-      for( let i = 0; i < numbers.length; i++ ){
-        x += +numbers[i]; // (*2)
-      }
-      return x;
-      *int.sort((a, b) => b - a);
-    /*for (int = 0; i < j.length; i++) {
-      sum += j[i];
-      }
-      return sum;*/
-
-      /*document.getElementById('result').innerHTML=i;
-    }*/
-     /* sum.value = sumArr( numbers );
-
-}
-function sumArr(arr){
-  let x = 0;
-  for( let i = 0; i < arr.length; i++ ){
-    x += +arr[i]; // (*2)
-  }
-  return x;
-}*/
-var sum=0;
-    var arr = [numbers];
-    for (var i in arr) {
-      sum += arr[i];
-          }
-          document.getElementById('result').innerHTML=sum;
-
-          for( let i = 0; i < numbs.length; i++ ){
-            numbers.push( numbs[i].value );
-            
-            numbs[i].addEventListener('numbers', function(){
-            numbers[i] = this.value;  
-            
-            });
-      }
+  arr.sort(function (a, b) {
+    return a - b;
+});*/
+/*let mas = [];
+          
+            for(i=0;i<=3;i++){
+                mas[i]=nums[i].value; // Из инпутов в массив
+            }
+            arr.sort((a, b) => b - a);
+            var result = {};
+    var elements = document.forms.foo.getElementsByTagName("input");
+    for(var i = 0; i < elements.length; i++)
+    {
+       /* do whatever you need to do with each input 
+    }let number = numbers.split(' ')
+    var arr = str.split(' ');*/
