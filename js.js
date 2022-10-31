75645fb5223410355e39ee352654fcfe0730854e
Oksanka848 <okmitka@mail.ru>
2022-10-31 10:14:14 +0000

    ch

--- js.js
+++ js.js
@@ -8,25 +8,26 @@
     
     let button = document.getElementById('button');
     button.onclick = sumInput;
-    let numbs = document.getElementById('numbers');
+    let numbs = document.getElementById('numbers').value;
     let all = document.getElementById('sort');
     let result  = document.getElementById('result');
     let numbers = [];
     
-    for( let i = 0; i < numbs.length; i++ ){
-      numbers.push( numbs[i].value );
-      small[i].addEventListener('input', function(){
-        numbers[i] = this.value; 
-        console.log(numbers);
-        
-      });
-    }  
-    console.log(numbers);
-function sumInput() {
-  result.value=numbers.reduce(function(sum, current) {
-    return sum + current;
-  }, 0);
-  all.value = numbers.join(', ');
-  document.getElementById('result').innerHTML=result.value;
-  document.getElementById('result').innerHTML=all.value;
-}
+   
+
+ 
+        function sumInput() {
+            let numbs = document.getElementById("numbers").value;
+            numbers = numbs.split(',');
+            result = numbers.reduce(sumInput2, 0);
+            console.log(numbers);
+            let sort=numbers.sort((a, b) => a - b);
+            console.log(sort);
+            all.innerHTML=sort.value;
+            
+        }
+ 
+        function sumInput2(total, value) {
+            return total + +value;
+            document.getElementById('result').innerHTML=result;
+        }
--- js2.js
+++ js2.js
@@ -18,3 +18,24 @@
        /* do whatever you need to do with each input 
     }let number = numbers.split(' ')
     var arr = str.split(' ');*/
+
+
+    for( let i = 0; i < numbs.length; i++ ){
+      numbers.push( numbs[i].value );
+      small[i].addEventListener('input', function(){
+        numbers[i] = this.value; 
+        console.log(numbers);
+        
+      });
+    }  
+    console.log(numbers);
+function sumInput() {
+
+  result.value=numbers.reduce(function(sum, current) {
+    return sum + current;
+  }, 0);
+  all.value = numbers.join(', ');
+  document.getElementById('result').innerHTML=result;
+  document.getElementById('result').innerHTML=all.value;
+}
+
